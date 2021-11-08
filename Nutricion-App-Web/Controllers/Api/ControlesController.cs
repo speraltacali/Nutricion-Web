e92d351ff9287f1108a12e199947cffe577ad7cf
Rober Albano <rober_albano@hotmail.com>
2021-11-07 21:51:06 -0300

    Se implementa vista controles con tabla

---
+++ Nutricion-App-Web/Controllers/Api/ControlesController.cs
@@ -0,0 +1,29 @@
+﻿using NA.IServicio.InformeAntropometrico;
+using NA.Servicio.InformeAntropometrico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nutricion_App_Web.Controllers.Api
+{
+    public class ControlesController : Controller
+    {
+        private readonly IInformeAntropometricoServicio _informeAntropometrico = new InformeAntropometricoServicio();
+        // GET: Controles
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Controles(long pacienteId=1)
+        {
+            var informes = _informeAntropometrico.ObtenerPorIdPaciente(pacienteId);
+
+            ViewData["Informes"] = informes;
+
+            return View();
+        }
+    }
+}
---
+++ Nutricion-App-Web/Controllers/Api/ProgresoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nutricion_App_Web.Controllers.Api
+{
+    public class ProgresoController : Controller
+    {
+        // GET: Progreso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Progreso()
+        {
+            return View();
+        }
+    }
+}
