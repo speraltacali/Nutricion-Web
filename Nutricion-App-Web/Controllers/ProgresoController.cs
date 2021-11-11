da33bca45e8fb1b9c9b57b8408acef198042825e
Rober Albano <rober_albano@hotmail.com>
2021-11-11 00:16:59 -0300

    Se implementa grafica de progreso con datos de db

--- Nutricion-App-Web/Controllers/ProgresoController.cs
+++ Nutricion-App-Web/Controllers/ProgresoController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using NA.IServicio.ContexturaCorporal;
+using NA.IServicio.InformeAntropometrico;
+using NA.Servicio.ContexturaCorporal;
+using NA.Servicio.InformeAntropometrico;
+using NA.Servicio.Token;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Nutricion_App_Web.Controllers
 {
     public class ProgresoController : Controller
     {
+        private readonly IInformeAntropometricoServicio _informeAntropometrico = new InformeAntropometricoServicio();
+        private readonly IContexturaCorporalServicio _contexturaCorporalServicio = new ContexturaCorporalServicio();
+
         // GET: Progreso
         public ActionResult Index()
         {
@@ -16,7 +22,33 @@ namespace Nutricion_App_Web.Controllers
 
         public ActionResult Progreso()
         {
-            return View();
+            var obtenerCookie = Request.Cookies["usuario"];
+
+            if (Validar.ValidarCookie(obtenerCookie == null ? "" : obtenerCookie["token"]))
+            {
+               var pacienteId = Convert.ToInt64(obtenerCookie["pacienteId"]);
+               var informes = _informeAntropometrico.ObtenerPorIdPaciente(pacienteId);
+
+                List<string> fechas = new List<string>();
+                List<int> porcentajeMusculo = new List<int>();
+                List<int> porcentajeGrasa = new List<int>();
+                List<string> peso = new List<string>();
+
+                foreach (var item in informes)
+                {
+                    fechas.Add(item.Fecha.ToShortDateString());
+                    porcentajeMusculo.Add(Convert.ToInt32(item.PorcentajeMusculo));
+                    porcentajeGrasa.Add(Convert.ToInt32(item.PorcentajeGrasa));                    
+                }             
+               
+
+                ViewData["InformesFechas"] = fechas;
+                ViewData["InformesPorcentajeMusculo"] = porcentajeMusculo;
+                ViewData["InformesPorcentajeGrasa"] = porcentajeGrasa;
+
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
